fa2623c37b90825298ea39b273f2fbb393299e86
mdsarfaraz307 <85289953+mdsarfaraz307@users.noreply.github.com>
2021-06-12 15:32:23 +0530

    Add files via upload

---
+++ assets/css/script.js
@@ -0,0 +1,8 @@
+
+	/*function myfunction() {
+		var str = prompt ("Hi ! My name is Md Sarfaraz and whats your name ", "")
+		alert ( "Nice to meet you " + str + "!")
+	}
+	<script>
+	alert( welcome )
+	</script>*/
